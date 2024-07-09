bdc2aaf566599d279f0adb9dcfc448829156ed4a
SyntaxErroredd <syntaxerror2227@gmail.com>
2024-07-09 13:34:19 +0800

    Added config reload and tab completer

--- src/main/java/me/syntaxerror/evodragons/commands/AdminCommands.java
+++ src/main/java/me/syntaxerror/evodragons/commands/AdminCommands.java
@@ -1,13 +1,18 @@
 package me.syntaxerror.evodragons.commands;
 
+import me.syntaxerror.evodragons.EvoDragons;
 import me.syntaxerror.evodragons.dragons.loot.ChooseDragonLootInventory;
 import me.syntaxerror.evodragons.dragons.loot.ChooseDragonTypeLootInventory;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
-public class AdminCommands implements CommandExecutor {
+import java.util.Arrays;
+import java.util.List;
+
+public class AdminCommands implements CommandExecutor, TabExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if(!(commandSender instanceof Player))
@@ -21,7 +26,26 @@ public class AdminCommands implements CommandExecutor {
             return false;
         if(strings[0].equalsIgnoreCase("loot")){
             new ChooseDragonTypeLootInventory(sender);
+            return true;
+        }
+        else if(strings[0].equalsIgnoreCase("reload")){
+            EvoDragons.getInstance().reloadConfig();
+            return true;
         }
         return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(!(commandSender instanceof Player))
+            return null;
+        Player sender = (Player) commandSender;
+        if(!sender.isOp())
+            return null;
+        if(!command.getName().equalsIgnoreCase("evodragons"))
+            return null;
+        if(strings.length == 1)
+            return Arrays.asList("reload", "loot");
+        return null;
     }
 }
